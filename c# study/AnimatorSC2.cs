e47cdbf6c80db22ec1320b52f2a8a953a92d0aeb
roylimtan <76837709+roylimtan@users.noreply.github.com>
2021-03-07 02:23:03 +0900

    Unity.animator

---
+++ c# study/AnimatorSC2.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorSC2 : MonoBehaviour
+{
+    /*
+     add component로 Animator추가 -> animation controller추가 -> Ainmator에 Controller에 추가
+
+    window->animation->각각의 애니메이션 만들기
+    window->animator->원하는 상태에 맞게 화살표 연결 -> parameters의 프러스버튼을 통해 조건 생성
+
+    Exit Time : 애니메이션이 얼만큼 끝나고 난 뒤에 상태 전이 할 지.
+    Transition Duration : 상태 전이하는데 얼마나 걸리게 할 지.
+    Interruption source : 언제 중단시킬지.
+     */
+
+    private Animator anim;
+
+    [SerializeField] private float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float _dirX = Input.GetAxisRaw("Horizontal"); //getaxisraw는 우측 좌측키에 따라서 1과 -1이 반환된다.
+        float _dirZ = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(_dirX, 0, _dirZ);
+
+        anim.SetBool("Right", false);
+        anim.SetBool("Left", false);
+        anim.SetBool("Up", false);
+        anim.SetBool("Down", false);
+
+        if (direction != Vector3.zero)
+        {
+            this.transform.Translate(direction.normalized * moveSpeed *Time.deltaTime);
+            //normalized를 하는 이유: 대각선의 움직임도 수평 수직 움직임과 똑같이 만들기 위해서 Normalized를한다.
+            //하지 않으면 대각선 속도만 빨라진다.
+
+            if (direction.x > 0)
+                anim.SetBool("Right", true);
+            else if (direction.x < 0)
+                anim.SetBool("Left", true);
+            else if (direction.z > 0)
+                anim.SetBool("Up", true);
+            else if (direction.z < 0)
+                anim.SetBool("Down", true);
+        }
+    }
+}
---
+++ c# study/AnimatorSC3.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorSC3 : MonoBehaviour
+{
+    /*
+     blend tree만들기
+    Animator에 새로운 blend tree 생성 -> 들어감 -> blend type변경 -> parameters 설정
+    -> 만든 애니메이션 넣고 값 입력
+
+    creat sub state machine으로 생성 -> 복잡한 애니메이터들을 넣어서 깔끔하게 정리
+
+    trigger는 그냥 실행되는 것
+     */
+
+    private Animator anim;
+    private Rigidbody rigid;
+    private BoxCollider col;
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private LayerMask layerMask;
+
+    private bool isMove;
+    private bool isJump;
+    private bool isFall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
+        col = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        TryWalk();
+        TryJump();
+
+    }
+
+    private void TryJump()
+    {
+        if(isJump)
+        {
+            if(rigid.velocity.y <= -0.1f && !isFall)
+            {
+                isFall = true;
+                anim.SetTrigger("Fall");
+            }
+
+            RaycastHit hitInfo;
+            if (Physics.Raycast(transform.position, -transform.up, out hitInfo, col.bounds.extents.y + 0.1f, layerMask))
+            {
+                anim.SetTrigger("Land");
+                isJump = false;
+                isFall = false;
+            }
+           
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && !isJump)
+        {
+            isJump = true;
+            rigid.AddForce(Vector3.up * jumpForce);
+            anim.SetTrigger("Jump");
+        }
+    }
+
+    private void TryWalk()
+    {
+        float _dirX = Input.GetAxisRaw("Horizontal");
+        float _dirZ = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(_dirX, 0, _dirZ);
+        isMove = false;
+
+        if (direction != Vector3.zero)
+        {
+            isMove = true;
+            this.transform.Translate(direction.normalized * moveSpeed * Time.deltaTime);
+        }
+
+        anim.SetBool("Move", isMove);
+        anim.SetFloat("DirX", direction.x);
+        anim.SetFloat("DirZ", direction.z);
+
+    }
+}
