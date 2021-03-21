f434b36651b489a1c58c06edd4fef6b11dc3d934
roylimtan <76837709+roylimtan@users.noreply.github.com>
2021-03-21 15:21:25 +0900

    Unity.texture

---
+++ c# study/Texturematerial.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Texturematerial : MonoBehaviour
+{
+
+    /*
+    메테리얼 생성하여 plane의 element에 넣는다.
+
+    메테리얼의 shader
+    (셰이더는 텍스처에 스크립트를 입혀서 물결치는 바다나, 눈밭에 발자국을 남길 수도 있다.)
+
+    particle
+    -additive : 생성한 파티클에 이 메테리얼을 입히고 이미지에 원하는 이미지를 넣어주면 된다.(불꽃)
+    -arpha blend : 연기표현할때 사용
+
+    standard
+    -opaque : 일반적인 텍스처
+    -cutout : 벌레파먹은 잎사귀 같은거
+    -fade : 반투명인간 표현
+    -transparent : 유리
+
+    metallic과 smoothness를 이용하여 빛과 진해지는 부분을 정해줄 수 있다.
+    emission : 어두운부분에 있어도 밝게 빛나게 한다.
+     */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
