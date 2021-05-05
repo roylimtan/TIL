549e9bfa428a262ac119e73d50247ff4c9ccf0fe
roylimtan <76837709+roylimtan@users.noreply.github.com>
2021-05-05 14:55:45 +0900

    2021.05.05

---
+++ FastCampus_Study/003_HelloWorld3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003_HelloWorld3
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("AAA{0} BBB{1}", 1, "bbb");
+            Console.Write("{0}{1}{2}", 1, 2, 3);
+            Console.WriteLine("static int Main(sting[] args)");
+            return 0;
+        }
+    }
+}
---
+++ FastCampus_Study/005_Check/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _005_Check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine("지금 듣는 강의는 " + args[0] + args[1] + "좋아요!!");
+            }
+            else
+            {
+                Console.WriteLine("HelloWorld: args.Length = 0");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+//커맨드창 겨기
+//cd..으로 초기화
+//주소 복붙
+//dir
+//실행하고자 하는 파일과 리스트에 넣을 값 입력
+//출력
