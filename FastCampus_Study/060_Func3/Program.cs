357173d3ae6db265dda9420574d347be9fabb01f
roylimtan <76837709+roylimtan@users.noreply.github.com>
2021-05-18 01:46:14 +0900

    2021.05.17

---
+++ FastCampus_Study/060_Func3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _060_Func3
+{
+    class Program
+    {
+        static int Square(int i)
+        {
+            int result = i;
+            return result * result;
+        }
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int resultA = Square(a); //F12, alt+f12
+            Console.WriteLine("resultA: " + resultA);
+
+            int b = 4;
+            int resultB = Square(b);
+            Console.WriteLine("resultB: " + resultB);
+
+            int resultC = Square(6);
+            Console.WriteLine("resultC: " + resultC);
+
+            resultC = Square(resultA * 3);
+            Console.WriteLine("resultC: " + resultC);
+        }
+    }
+}
---
+++ FastCampus_Study/CheckPoint01/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace CheckPoint01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string LINE = "=============================================";
+            const int END_LINE = 42;
+            const int DELAY_TIME = 200;
+
+            int runA = 0;
+            int runB = 0;
+            int runC = 0;
+            int runD = 0;
+            while (true)
+            {
+                Random rnd = new Random();
+
+                Thread.Sleep(DELAY_TIME); //딜레이
+                Console.Clear();
+
+                ++runA;
+                ++runB;
+                ++runC;
+                ++runD;
+
+                int rndNum = rnd.Next(0, 4);
+
+                switch(rndNum)
+                {
+                    case 0:
+                        ++runA;
+                        break;
+
+                    case 1:
+                        ++runB;
+                        break;
+
+                    case 2:
+                        ++runC;
+                        break;
+
+                    case 3:
+                        ++runD;
+                        break;
+                }
+
+                Console.WriteLine(LINE);             
+
+                for (int i = 0; i < runA; i++)
+                    Console.Write(" ");
+                Console.Write("1");
+
+                for (int i = (END_LINE - 2) - runA; i >= 0; i--)
+                    Console.Write(" ");
+                Console.WriteLine("|");
+
+                for (int i = 0; i < runB; i++)
+                    Console.Write(" ");
+                Console.Write("2");
+
+                for (int i = (END_LINE - 2) - runB; i >= 0; i--)
+                    Console.Write(" ");
+                Console.WriteLine("|");
+
+                for (int i = 0; i < runC; i++)
+                    Console.Write(" ");
+                Console.Write("3");
+
+                for (int i = (END_LINE - 2) - runC; i >= 0; i--)
+                    Console.Write(" ");
+                Console.WriteLine("|");
+
+                for (int i = 0; i < runD; i++)
+                    Console.Write(" ");
+                Console.Write("4");
+
+                for (int i = (END_LINE - 2) - runD; i >= 0; i--)
+                    Console.Write(" ");
+                Console.WriteLine("|");
+
+                Console.WriteLine(LINE);
+
+                if(runA >= END_LINE || runB >= END_LINE || runC >= END_LINE || runD >= END_LINE)
+                {
+                    string strResult = "결과: ";
+
+                    if(runA >= END_LINE)
+                    {
+                        strResult += "!! 1번선수 우승 !!";
+                    }
+
+                    else if(runB >= END_LINE)
+                    {
+                        strResult += "!! 2번선수 우승 !!";
+                    }
+
+                    else if (runC >= END_LINE)
+                    {
+                        strResult += "!! 3번선수 우승 !!";
+                    }
+
+                    else
+                    {
+                        strResult += "!! 4번선수 우승 !!";
+
+                    }
+
+                    Console.WriteLine(strResult);
+
+                    Console.Write("다시하시려면 0번 입력: ");
+                    if (0 == int.Parse(Console.ReadLine()))
+                    {
+                        runA = 0;
+                        runB = 0;
+                        runC = 0;
+                        runD = 0;
+                    }
+
+                    else
+                        break;
+                }
+            }
+            
+        }
+    }
+}
