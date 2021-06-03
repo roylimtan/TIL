f9c2b35ff67e4895b3e948739c7a384d0d3561f8
roylimtan <76837709+roylimtan@users.noreply.github.com>
2021-06-03 15:15:30 +0900

    2021.06.03

---
+++ FastCampus_Study/083_override/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//같은 기능을 하는 함수를 오버라이딩해서 다른 기능을 추가적으로 할 수 있도록...
+
+namespace _083_override
+{
+    class Super
+    {
+        protected int num;
+
+        public virtual void Print()
+        {
+            Console.WriteLine("num: {0}", num);
+        }
+    }
+
+    class AA : Super
+    {
+        public int a;
+
+        public override void Print()
+        {
+            
+           base.Print();
+           Console.WriteLine("AA a: {0}", a);
+           
+        }
+    }
+
+    class BB: Super
+    {
+        public int b;
+
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine("BB b:{0}", b);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Super super = new Super();
+            super.Print();
+
+            Super aa = new AA();
+            aa.Print();
+
+            Super bb = new BB();
+            bb.Print();
+        }
+    }
+}
---
+++ FastCampus_Study/088_struct/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _088_struct
+{
+    struct AA
+    {
+        public int x;
+        public int y;
+
+        //public AA() 오류: 파라미터 없음
+        public AA(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("x: {0}, y: {1}", x, y);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AA aa = new AA(10, 20);
+            aa.x = 100;
+            aa.Print();
+
+            AA aaa;
+            aaa.x = 100;
+            aaa.y = 200;
+            aaa.Print();
+
+            AA copyAA = aa; //value type
+            copyAA.x = 10000;
+            copyAA.y = 20000;
+            copyAA.Print();
+
+            aa.Print();
+        }
+    }
+}
