1e92019afbd1ec2ba58f5fa922ddc0cca0e49b92
roylimtan <76837709+roylimtan@users.noreply.github.com>
2021-06-04 19:22:07 +0900

    2021.06.04

---
+++ FastCampus_Study/089_interface/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _089_interface
+{
+    interface IAA
+    {
+        //public, private, 필드 불가
+        int A { get; set; }
+        void IAAPrint();
+    }
+
+    interface IBB
+    {
+        void IBBPrint();
+    }
+
+    class Super
+    {
+        protected int num;
+        public virtual void Print()
+        {
+            Console.WriteLine("=================================================");
+        }
+    }
+
+    class AA : IAA
+    {
+        public int A { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void IAAPrint()
+        {
+            Console.WriteLine("class AA interface IAA에 IAAPrint () 재정의");
+        }
+
+        public void IBBPrint()
+        {
+            Console.WriteLine("class AA interface IBB에 IBBPrint() 재정의");
+        }
+    }
+
+    class BB : IAA, IBB
+    {
+        public int A
+        {
+            get { return A; }
+            set { A = value; }
+        }
+
+        public void IAAPrint()
+        {
+            Console.WriteLine("class BB interface IAA에 IAAPrint() 재정의");
+        }
+
+        public void IBBPrint()
+        {
+            Console.WriteLine("class BB interface IBB에 IBBPrint() 재정의");
+        }
+    }
+
+    class CC : Super, IAA, IBB
+    {
+        public int A
+        {
+            get { return A; }
+            set { A = value; }
+        }
+
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine("class Super => print() 재정의");
+        }
+
+        public void IAAPrint()
+        {
+            Console.WriteLine("class CC interface IAA에 IAAPrint() 재정의");
+        }
+
+        public void IBBPrint()
+        {
+            Console.WriteLine("class CC interface IBB에 IBBPrint() 재정의");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AA aa = new AA();
+            aa.IAAPrint();
+
+            BB bb = new BB();
+            bb.IAAPrint();
+            bb.IBBPrint();
+
+            IAA iaa = new AA(); //참조 가능
+            iaa.IAAPrint();
+
+            IBB ibb = bb as IBB;
+            ibb.IBBPrint();
+
+            CC cc = new CC();
+            cc.Print();
+            cc.IAAPrint();
+            cc.IBBPrint();
+
+
+            Super scc = cc as Super;
+            scc.Print();
+
+            IAA iaacc = cc as IAA;
+            iaacc.IAAPrint();
+
+            IBB ibbcc = cc as IBB;
+            ibbcc.IBBPrint();
+        }
+    }
+}
---
+++ FastCampus_Study/090_abstract/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+abstract class abstractAA
+{
+    protected int num;
+
+    public abstractAA(int num)
+    {
+        this.num = num;
+    }
+
+    public abstract void abstractPrint();
+
+    public virtual void virtualPrint()
+    {
+        Console.WriteLine("abstractAA virtual void virtualPrint()");
+    }
+
+    public void Print()
+    {
+        Console.WriteLine("abstractAA pritn()");
+    }
+}
+
+class AA : abstractAA
+{
+    public AA(int num) : base(num)
+    {
+        Console.WriteLine("num: {0}", num);
+    }
+
+    public override void abstractPrint()
+    {
+        Console.WriteLine("AA override void abstractPrint()");
+    }
+
+    public override void virtualPrint()
+    {
+        base.virtualPrint();
+        Console.WriteLine("AA override void virtualPrint()");
+    }
+}
+
+namespace _090_abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AA aa = new AA(10);
+            aa.abstractPrint();
+            aa.virtualPrint();
+            aa.Print();
+
+            //abstractAA aaa = new abstractAA(10); 불가능
+        }
+    }
+}
