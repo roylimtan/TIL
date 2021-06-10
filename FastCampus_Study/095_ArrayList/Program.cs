d6cfc208a0f5fde5f0047bd5cfc3abbd6fde6150
roylimtan <76837709+roylimtan@users.noreply.github.com>
2021-06-11 00:50:29 +0900

    2021.06.10

---
+++ FastCampus_Study/095_ArrayList/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace _095_ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrList = new ArrayList();
+
+            arrList.Add("Hello");
+            arrList.Add(10f);
+
+            for(int i = 0; i < 10; i++)
+            {
+                arrList.Add(i);
+            }
+
+            foreach(object data in arrList)
+            {
+                Console.WriteLine("arrKist data: " + data);
+            }
+
+            //배열데이터로 초기화
+            Console.WriteLine("배열데이터로 초기화");
+            int[] arrData = { 100, 200, 300 };
+            ArrayList arrCopyList = new ArrayList(arrData);
+
+            foreach(object data in arrCopyList)
+            {
+                Console.WriteLine("arrCopyList data: " + data);
+            }
+        }
+    }
+}
---
+++ FastCampus_Study/098_Hashtable/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace _098_Hashtable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add("pos", 10);
+            hashtable.Add("name", "jack");
+            hashtable["weight"] = 10.8f;
+
+            foreach(object key in hashtable.Keys)
+            {
+                Console.WriteLine("key: {0}, data: {1}", key, hashtable[key]);
+            }
+
+            Console.WriteLine("");
+
+            Hashtable hashTableCopy = new Hashtable()
+            {
+                ["pos"] = 10,
+                ["name"] = "jack",
+                ["weight"] = 10.8f,
+            };
+
+            foreach(object key in hashTableCopy.Keys)
+            {
+                Console.WriteLine("key: {0}, data: {1}", key, hashTableCopy[key]);
+            }
+        }
+    }
+}
