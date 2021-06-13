9749ca87a78c9875dcc4ca58e8d72e378c5ab089
roylimtan <76837709+roylimtan@users.noreply.github.com>
2021-06-14 02:27:23 +0900

    2021.06.13

---
+++ FastCampus_Study/101_Check/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+//성적 프로그램을 3명까지만 저장하고 정보 검ㅁ색이 가능한 프로그램
+//저장했던 자료에서 참고하고 싶은 학생번호로 정보 보여주기
+//ContainKey()함수 참고: Hashtable에 저장된 키값이 있는지 체크
+
+namespace _101_Check
+{
+    class Program
+    {
+        static int CheckID(int id, Hashtable hashTable)
+        {
+            if(hashTable.ContainsKey(id))
+            {
+                return id;
+            }
+
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            int id;
+            int kor;
+            int math;
+            int eng;
+            int Num;
+            int Count = 1;
+            int Studentnum;
+            int SellID;
+            Hashtable KorhashTable = new Hashtable();
+            Hashtable MathhashTable = new Hashtable();
+            Hashtable EnghashTable = new Hashtable();
+            Hashtable TotalhashTable = new Hashtable();
+            Hashtable AvehashTable = new Hashtable();
+
+
+            while (true)
+            {
+                Console.Write("학생 ID를 입력하세요.");
+                id = int.Parse(Console.ReadLine());
+
+
+                Console.Write("국어 점수를 입력하세요.");
+                kor = int.Parse(Console.ReadLine());
+
+                Console.Write("수학 점수를 입력하세요.");
+                math = int.Parse(Console.ReadLine());
+
+                Console.Write("영어 점수를 입력하세요.");
+                eng = int.Parse(Console.ReadLine());
+
+                KorhashTable.Add(id, kor);
+                MathhashTable.Add(id, math);
+                EnghashTable.Add(id, eng);
+                TotalhashTable.Add(id, (kor + math + eng));
+                AvehashTable.Add(id, (kor + math + eng) / 3);
+
+                Console.Write("(0)나가기  (1)학생 추가");
+                Num = int.Parse(Console.ReadLine());
+
+                if (Num == 1)
+                {
+                    Count++;
+                    Console.WriteLine("");
+                }
+                else
+                {
+                    Console.Clear();
+                    break;
+                }
+            }
+
+            while (true)
+            {
+                foreach (object key in KorhashTable.Keys)
+                    Console.WriteLine("학생ID:  {0}", key);
+
+                Console.Write("찾으시는 학생 ID를 입력하세요. (0)나가기");
+                Studentnum = int.Parse(Console.ReadLine());
+
+                if (Studentnum == 0)
+                    break;
+
+                SellID = CheckID(Studentnum, KorhashTable);
+
+                if(SellID >= 0)
+                {
+                    Console.WriteLine("찾으려는 학생 ID: {0}", SellID);
+                    Console.WriteLine("국어점수: {0}", KorhashTable[SellID]);
+                    Console.WriteLine("수학점수: {0}", MathhashTable[SellID]);
+                    Console.WriteLine("영어점수: {0}", EnghashTable[SellID]);
+                    Console.WriteLine("총점: {0}", TotalhashTable[SellID]);
+                    Console.WriteLine("평균: {0}", AvehashTable[SellID]);
+                    Console.WriteLine("");
+                }
+            }
+        }
+    }
+}
---
+++ FastCampus_Study/102_Check/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+//입력 받은두 수의 결과를 저장하는 프로그램
+//제한 없이 계속 입력 받을 수 있음
+//저장했던 자료를 모두 보여주기
+
+namespace _102_Check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue Aqueue = new Queue();
+            Queue Bqueue = new Queue();
+            Queue Resultqueue = new Queue();
+
+            int a;
+            int b;
+            int num;
+
+            while(true)
+            {
+                Console.Write("첫 번째 수를 입력하세요. ");
+                a = int.Parse(Console.ReadLine());
+
+                Aqueue.Enqueue(a);
+
+                Console.Write("두 번째 수를 입력하세요. ");
+                b = int.Parse(Console.ReadLine());
+
+                Bqueue.Enqueue(b);
+
+                Resultqueue.Enqueue(a + b);
+
+                Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
+                Console.WriteLine("");
+                Console.Write("계속 하시겠습니까? (0)나가기 (1)계속");
+                num = int.Parse(Console.ReadLine());
+
+                if(num == 1)
+                {
+                    Console.WriteLine("");
+                }
+                else
+                {
+                    Console.Clear();
+                    break;
+                }
+            }
+
+            Console.WriteLine("========결과 공개============");
+
+            while (Aqueue.Count > 0)
+            {
+                Console.WriteLine("{0} + {1} = {2}", Aqueue.Dequeue(), Bqueue.Dequeue(), Resultqueue.Dequeue());
+            }
+        }
+    }
+}
